1ff360cbdbb3953283302969394467baad7b3ec7
David Shorthouse <davidpshorthouse@gmail.com>
2016-03-30 09:14:07 -0400

    Remove csv files, reorganize bin directories

--- bin/all/non-code-scientific.rb
+++ bin/all/non-code-scientific.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-require_relative '../environment.rb'
+require_relative '../../environment.rb'
 
 #Issue: https://trello.com/c/ZpAqA6HN
 
--- bin/botany/xx-variations.rb
+++ bin/botany/xx-variations.rb
@@ -5,8 +5,7 @@ require_relative '../../environment.rb'
 #Issue: https://trello.com/c/JOeYqhyC
 
 results = []
-output_dir = File.dirname(__FILE__) + "/output/"
-CSV.open(output_dir + "xx-variations.csv", 'w') do |csv|
+CSV.open(File.dirname(__FILE__) + "/xx-variations.csv", 'w') do |csv|
   csv << ["speckey", "scientific_name", "variation"]
   TaxonVariation.search_by_prefix("xx").each do |tv|
     if tv.taxon.collection == "Botany" && tv.scientific_name[2..-1] == tv.taxon.scientific_name
