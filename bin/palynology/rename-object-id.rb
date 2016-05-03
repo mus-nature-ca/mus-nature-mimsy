50110a9af8844b5cfb5c5fa5436805e364ae8891
David Shorthouse <davidpshorthouse@gmail.com>
2016-05-03 10:37:36 -0400

    Palynology object id rename

--- bin/palynology/rename-object-id.rb
+++ bin/palynology/rename-object-id.rb
@@ -3,13 +3,13 @@
 require_relative '../../environment.rb'
 include Sinatra::Mimsy::Helpers
 
-catalogs = Catalog.where(id_prefix: "CMNPB")
+catalogs = Catalog.where(id_prefix: "CMNPYF")
 
 pbar = ProgressBar.new("ZERO-PAD", catalogs.count)
 count = 0
 
 catalogs.find_each do |catalog|
-  new_id = catalog.id_number.gsub(/([A-Z]+)\s+0+(\d+)/, '\1 \2')
+  new_id = catalog.id_number.gsub(/([A-Z]+)(\d+)/, '\1 \2').gsub(/([A-Z]+)\s0+(\d+)/, '\1 \2')
   if new_id != catalog.id_number
     catalog.id_number = new_id
     catalog.save
