a85b9d2135ff24df766e9d9a3ba6e37469109cd8
David Shorthouse <davidpshorthouse@gmail.com>
2016-05-20 09:52:53 -0400

    Multifield substrate clean-up

--- bin/all/multifield-clean-up.rb
+++ bin/all/multifield-clean-up.rb
@@ -3,13 +3,16 @@
 require_relative '../../environment.rb'
 include Sinatra::Mimsy::Helpers
 
-CatalogMultifield.find_in_batches do |batches|
-  batches.each do |cm|
-    if cm.descriptor == "substrate" && !cm.note.nil? && cm.term == "n/a"
-      puts [cm.mkey, cm.note] 
-#      cm.term = cm.note
-#      cm.note = nil
-#      cm.save
+CSV.open(output_dir(__FILE__) + "/multifield-clean-up.csv", 'w') do |csv|
+  csv << ["mkey", "descriptor", "term", "note"]
+  CatalogMultifield.find_in_batches do |batches|
+    batches.each do |cm|
+      if cm.descriptor == "substrate" && !cm.note.nil? && cm.term == "n/a"
+        csv << [cm.mkey, cm.descriptor, cm.term, cm.note]
+        cm.term = cm.note
+        cm.note = nil
+        cm.save
+      end
     end
   end
 end
