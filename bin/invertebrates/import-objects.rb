f7550094337d123f827f648b29b30e7c2a383a1b
David Shorthouse <davidpshorthouse@gmail.com>
2017-02-21 16:04:10 -0500

    Complete scripts for importing inverts spreadsheet

--- bin/invertebrates/import-objects.rb
+++ bin/invertebrates/import-objects.rb
@@ -3,34 +3,60 @@
 require_relative '../../environment.rb'
 include Sinatra::Mimsy::Helpers
 
-file = "/Users/dshorthouse/Desktop/inverts-import-objects.csv"
+file = "/Users/dshorthouse/Desktop/inverts-import-objects-utf16.txt"
 
-CSV.foreach(file, :headers => true) do |row|
+CSV.foreach(file, :headers => true, :col_sep => "\t", :encoding => 'bom|utf-16le:utf-8') do |row|
+
+  puts row["CATALOGUE.ID_NUMBER"]
+
+  #Get the linked site
+  site = Site.find_by_site_id(row["SITES.SITE_ID"].strip)
+
+  #Get the linked place
+  place_collected = row["CATALOGUE.PLACE_COLLECTED"].strip rescue nil
+
   #create new catalog record
   obj = Catalog.new
   obj.catalog_number = row["CATALOGUE.ID_NUMBER"].strip
   obj.collection = row["CATALOGUE.CATEGORY1"].strip
   obj.scientific_name = row["CATALOGUE.ITEM_NAME"].strip
   obj.scientific_name[0] = obj.scientific_name[0].capitalize
-  obj.whole_part = "whole"
-  obj.item_count = row["CATALOGUE.ITEM_COUNT"].strip
-  obj.sex = row["CATALOGUE.SEX"].strip
-  obj.collector = row["CATALOGUE.COLLECTOR"].strip
-  obj.date_collected = row["CATALOGUE.DATE_COLLECTED"].strip
-  obj.description = row["CATALOGUE.DESCRIPTION"].strip
-  obj.note = row["CATALOGUE.NOTE"].strip
+  obj.whole_part = row["CATALOGUE.WHOLE_PART"].strip rescue nil
+  obj.materials = row["CATALOGUE.MATERIALS"].strip rescue nil
+  obj.item_count = row["CATALOGUE.ITEM_COUNT"].strip.to_i rescue nil
+  obj.sex = row["CATALOGUE.SEX"].strip rescue nil
+  obj.collector = row["CATALOGUE.COLLECTOR"].strip rescue nil
+  obj.date_collected = row["CATALOGUE.DATE_COLLECTED"].strip rescue nil
+  obj.description = row["CATALOGUE.DESCRIPTION"].strip rescue nil
+  obj.note = row["CATALOGUE.NOTE"].strip rescue nil
+  obj.site = site.site_id
+  obj.place_collected = place_collected
+  obj.gbif = true
   obj.save
 
   #Find object again - damn Oracle!
   obj = Catalog.find_by_catalog_number(obj.catalog_number)
 
   #create link to Taxonomy
-  tax = Taxon.find_by_scientific_name(row["ITEMS_TAXONOMY.TAXONOMY"].strip)
-  obj_tax = CatalogTaxon.new
-  obj_tax.taxon_id = tax.id
-  obj_tax.catalog_id = obj.id
-  obj_tax.attributor = row["ITEMS_TAXONOMY.ATTRIBUTOR"].strip
-  obj_tax.save
+  taxon = row["ITEMS_TAXONOMY.TAXONOMY"].strip
+  tax = Taxon.find_by_scientific_name(taxon)
+  #Find a variant if verbatim not found
+  if tax.nil?
+    tax = TaxonVariation.find_by_scientific_name(taxon).taxon rescue nil
+  end
+  if !tax.nil?
+    obj_tax = CatalogTaxon.new
+    obj_tax.taxon_id = tax.id
+    obj_tax.catalog_id = obj.id
+    obj_tax.attributor = row["ITEMS_TAXONOMY.ATTRIBUTOR"].strip rescue nil
+    obj_tax.save
+  end
+
+  #create link to Catalog Name
+  cn = CatalogName.new
+  cn.scientific_name = obj.scientific_name
+  cn.catalog_id = obj.id
+  cn.save
 
   #create link to Person (collector)
   pers = Person.find_by_preferred_name(obj.collector)
@@ -40,17 +66,23 @@ CSV.foreach(file, :headers => true) do |row|
   cat_coll.relationship = "collector"
   cat_coll.save
 
+  #create link to collected date
+  cd = CollectedDate.new
+  cd.catalog_id = obj.id
+  cd.date_text = obj.date_collected
+  cd.save
+
   #measurements
   (1..3).each do |num|
-    part = row["MEASUREMENTS.PART_MEASURED_#{num}"].strip rescue nil
+    part = row["OTHER_MEASUREMENTS.PART_MEASURED_#{num}"].strip rescue nil
     if !part.nil?
-      measurement = CatalogMeasurement.new
-      measurement.catalog_id = obj.id
-      measurement.part_measured = part.downcase
-      measurement.dimension1 = row["MEASUREMENTS.DIMENSION1_#{num}"].strip
-      measurement.measurement_type = row["MEASUREMENTS.MEASUREMENT_TYPE_#{m}"].strip.downcase
-      measurement.unit1 = row["MEASUREMENTS.UNIT1_#{m}"].strip
-      measurement.save
+      om = CatalogOtherMeasurement.new
+      om.catalog_id = obj.id
+      om.part_measured = part.downcase
+      om.measurement = row["OTHER_MEASUREMENTS.MEASUREMENT_#{num}"].strip rescue nil
+      om.om_type = row["OTHER_MEASUREMENTS.OM_TYPE_#{num}"].strip.downcase rescue nil
+      om.unit = row["OTHER_MEASUREMENTS.UNIT_#{num}"].strip rescue nil
+      om.save
     end
   end
 
@@ -58,16 +90,27 @@ CSV.foreach(file, :headers => true) do |row|
   if row["OTHER_NUMBERS.OTHER_NUMBER"]
     other_number = CatalogOtherNumber.new
     other_number.catalog_id = obj.id
-    other_number.other_number = row["OTHER_NUMBERS.OTHER_NUMBER"].strip
-    other_number.on_type = row["OTHER_NUMBERS.ON_TYPE"].strip.downcase
+    other_number.other_number = row["OTHER_NUMBERS.OTHER_NUMBER"].strip rescue nil
+    other_number.on_type = row["OTHER_NUMBERS.ON_TYPE"].strip.downcase rescue nil
     other_number.save
   end
 
   #link to site
-  site = Site.find_by_site_id(row["SITES.SITE_ID"].strip)
   catsite = CatalogSite.new
   catsite.catalog_id = obj.id
   catsite.site_id = site.id
   catsite.save
+
+  #link to place collected
+  if !place_collected.nil?
+    split_place = place_collected.split(": ")
+    place = Place.where("broader_text LIKE '%#{split_place[0]}'").where(place1: split_place[1])
+    if place.count == 1
+      collplace = CatalogCollectionPlace.new
+      collplace.catalog_id = obj.id
+      collplace.place_id = place.first.id
+      collplace.save
+    end
+  end
 
 end
--- bin/invertebrates/import-sites.rb
+++ bin/invertebrates/import-sites.rb
@@ -3,40 +3,55 @@
 require_relative '../../environment.rb'
 include Sinatra::Mimsy::Helpers
 
-file = "/Users/dshorthouse/Desktop/inverts-import-sites.csv"
+file = "/Users/dshorthouse/Desktop/inverts-import-sites-utf16.txt"
 
-CSV.foreach(file, :headers => true) do |row|
+CSV.foreach(file, :headers => true, :col_sep => "\t", :encoding => 'bom|utf-16le:utf-8') do |row|
+
   #create new site record
   site = Site.new
   site.site_id = row["SITES.SITE_ID"].strip
   site.site_name = row["SITES.SITE_NAME"].strip
-  site.site_date = row["SITES.SITE_DATE"].strip
-  site.description = row["SITES.SITE_DESCRIPTION"].strip
-  site.register_status = row["SITES.REGISTER_STATUS"].strip
-  site.start_latitude = row["SITES.START_LATITUDE"].strip
-  site.start_latitude_dec = row["SITES.START_LATITUDE"].strip
-  site.start_longitude = row["SITES.START_LONGITUDE"].strip
-  site.start_longitude_dec = row["SITES.START_LONGITUDE"].strip
+  site.site_date = row["SITES.SITE_DATE"].strip rescue nil
+  site.description = row["SITES.DESCRIPTION"].strip rescue nil
+  site.register_status = row["SITES.REGISTER_STATUS"].strip rescue nil
+  site.decimal_is_primary = true
+  site.start_latitude = row["SITES.START_LATITUDE"].strip.to_f rescue nil
+  site.start_latitude_dec = row["SITES.START_LATITUDE"].strip.to_f rescue nil
+  site.start_longitude = row["SITES.START_LONGITUDE"].strip.to_f rescue nil
+  site.start_longitude_dec = row["SITES.START_LONGITUDE"].strip.to_f rescue nil
+  site.environment = row["SITES.ENVIRONMENT"].strip rescue nil
+  site.publish = true
+  site.location = row["SITES.LOCATION"].strip rescue nil
+  site.location_accuracy = row["SITES.LOCATION_ACCURACY"].strip rescue nil
+  site.note = row["SITES.NOTE"] rescue nil
   site.save
 
   #query for created site - damn Oracle!
   site = Site.find_by_site_id(site.site_id)
 
-  #create site coordinates
-  ["latitude", "longitude"].each do |type|
-    site_coordinate = SiteCoordinate.new
-    site_coordinate.site_id = site.id
-    site_coordinate.coord_type = "start #{type}"
-    site_coordinate.decimal_coordinate = site.send("start_#{type}_dec")
-    site_coordinate.decimal_is_primary = true
-    site_coordinate.save
+  #fix coordinate text
+  if !site.start_latitude.nil? || !site.start_longitude.nil?
+    if !site.start_latitude.nil?
+      site.start_latitude.gsub!(",", ".")
+    end
+    if !site.start_longitude.nil?
+      site.start_longitude.gsub!(",", ".")
+    end
+    site.save
+  end
+
+  site.coordinates.each do |coord|
+    coord.coord_type.downcase!
+    coord.save
   end
 
   #create other numbers
   other_number = SiteOtherNumber.new
   other_number.site_id = site.id
-  other_number.other_number = row["SITE_OTHER_NUMBERS.OTHER_NUMBER"].strip
-  other_number.save
+  other_number.other_number = row["SITE_OTHER_NUMBERS.OTHER_NUMBER"].strip rescue nil
+  if !other_number.other_number.nil?
+    other_number.save
+  end
 
   #create measurements
   (1..12).each do |m|
