dc74a934ef61584ea4de4c601f351a9b7a138eda
David Shorthouse <davidpshorthouse@gmail.com>
2017-04-27 13:10:14 -0400

    Modify media upload script to look for file on server

--- bin/all/multimedia-upload.rb
+++ bin/all/multimedia-upload.rb
@@ -3,59 +3,71 @@
 require_relative '../../environment.rb'
 include Sinatra::Mimsy::Helpers
 
-file = "/Users/dshorthouse/Desktop/MXG_Media_Upload_Brouillet-utf16.txt"
-log = "/Users/dshorthouse/Desktop/MXG_Media_Upload_Brouillet-log.csv"
+file = "/Users/dshorthouse/Desktop/MXG_Media_Upload_Luc_Brouillet.txt"
+log = "/Users/dshorthouse/Desktop/MXG_Media_Upload_Luc_Brouillet-log.csv"
 
-missing_catalogs = []
+missing = []
 
 CSV.foreach(file, :headers => true, :col_sep => "\t", :encoding => 'bom|utf-16le:utf-8') do |row|
 
   next if row["MEDIA.MEDIA_ID"].nil?
 
+  puts row["MEDIA.MEDIA_ID"]
+
   #Get the linked object
   catalog = Catalog.find_by_catalog_number(row["MEDIA_ITEMS.OBJECT"].strip)
 
   if catalog.nil?
-    puts row["MEDIA.MEDIA_ID"]
-    missing_catalogs << [row["MEDIA_ITEMS.OBJECT"], "missing"]
+    missing << [row["MEDIA_ITEMS.OBJECT"], "catalog missing"]
   else
 
-    m = Medium.new
+    #check if image exists
+    path = row["MEDIA.LOCATOR"].gsub(/\\+/, '/').sub("n-nas1.mus-nature.ca","Volumes")
+    if !File.exists?(File.join(path, row["MEDIA.MEDIA_ID"]))
+      missing << [row["MEDIA.MEDIA_ID"], "image missing"]
+    end
 
-    m.media_id = row["MEDIA.MEDIA_ID"].strip
-    m.locator = row["MEDIA.LOCATOR"].strip rescue nil
-    m.record_type = row["MEDIA.RECORD_TYPE"].strip rescue nil
-    m.media = row["MEDIA.MEDIA"].strip rescue nil
-    m.format = row["MEDIA.FORMAT"].strip rescue nil
-    m.thumbnail = (row["MEDIA.THUMBNAIL"].strip == "Y") ? true : false rescue nil
-    m.date_captured = row["MEDIA.DATE_CAPTURED"].strip rescue nil
-    m.capture_method = row["MEDIA.CAPTURE_METHOD"].strip rescue nil
-    m.captured_by = row["MEDIA.CAPTURED_BY"].strip rescue nil
-    m.media_type = row["MEDIA.MEDIA_TYPE"].strip rescue nil
-    m.credit_line = row["MEDIA.CREDIT_LINE"].strip rescue nil
-    m.caption = row["MEDIA.CAPTION"].strip rescue nil
-    m.repro_allowed = (row["MEDIA.REPRO_ALLOWED"].strip == "Y") ? true : false rescue nil
-    m.publish = (row["Publish"].strip == "Y") ? true : false rescue nil
-    m.physical_location = row["MEDIA.PHYSICAL_LOCATION"].strip rescue nil
-    m.location_date = row["MEDIA.LOCATION_DATE"].strip rescue nil
-    m.save
+    begin
+      #Save new media record
+      m = Medium.new
+      m.media_id = row["MEDIA.MEDIA_ID"].strip
+      m.locator = row["MEDIA.LOCATOR"].strip rescue nil
+      m.record_type = row["MEDIA.RECORD_TYPE"].strip rescue nil
+      m.media = row["MEDIA.MEDIA"].strip rescue nil
+      m.format = row["MEDIA.FORMAT"].strip rescue nil
+      m.thumbnail = (row["MEDIA.THUMBNAIL"].strip == "Y") ? true : false rescue nil
+      m.date_captured = row["MEDIA.DATE_CAPTURED"].strip rescue nil
+      m.capture_method = row["MEDIA.CAPTURE_METHOD"].strip rescue nil
+      m.captured_by = row["MEDIA.CAPTURED_BY"].strip rescue nil
+      m.media_type = row["MEDIA.MEDIA_TYPE"].strip rescue nil
+      m.credit_line = row["MEDIA.CREDIT_LINE"].strip rescue nil
+      m.caption = row["MEDIA.CAPTION"].strip rescue nil
+      m.repro_allowed = (row["MEDIA.REPRO_ALLOWED"].strip == "Y") ? true : false rescue nil
+      m.publish = (row["Publish"].strip == "Y") ? true : false rescue nil
+      m.physical_location = row["MEDIA.PHYSICAL_LOCATION"].strip rescue nil
+      m.location_date = row["MEDIA.LOCATION_DATE"].strip rescue nil
+      m.save
 
-    #reload Medium
-    medium = Medium.find_by_media_id(m.media_id)
+      #reload Medium
+      medium = Medium.find_by_media_id(m.media_id)
 
-    cm = CatalogMedium.new
-    cm.medium_id = medium.id
-    cm.catalog_id = catalog.id
-    cm.sort = row["MEDIA_ITEMS.STEP"].strip rescue nil
-    cm.save
+      #save new CatalogMedium record
+      cm = CatalogMedium.new
+      cm.medium_id = medium.id
+      cm.catalog_id = catalog.id
+      cm.sort = row["MEDIA_ITEMS.STEP"].strip rescue nil
+      cm.save
+    rescue
+      missing << [row["MEDIA.MEDIA_ID"], "failed"]
+    end
 
   end
 
 end
 
 CSV.open(log, 'w') do |csv|
-  csv << ["MEDIA_ITEMS.OBJECT", "Note"]
-  missing_catalogs.each do |item|
+  csv << ["Item", "Note"]
+  missing.each do |item|
     csv << item
   end
 end
